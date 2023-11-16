a7b2f44a39ecb0ac0a431394cae3cfdb63101ba8
VojtechKaupa68 <151013617+VojtechKaupa68@users.noreply.github.com>
2023-11-16 10:49:43 +0100

    summary

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__, static_url_path='/static', static_folder='static', template_folder='templates')
+
+
+@app.route('/formular', methods=['GET', 'POST'])
+def index():
+    return render_template('formular.html'), 200
+
+
+@app.route('/registrace', methods=['GET', 'POST'])
+def registrace_stranka():
+    if request.method == 'GET':
+        return render_template('registrace.html'), 200
+    if request.method == 'POST':
+        nickname = request.form['nick']
+        friend = request.form['kanoe_kamarad']
+        print(nickname + ' ' + friend)
+        return render_template('index.html'), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
